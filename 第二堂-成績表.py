1693c9129426340a7f2cba84074d636109ea7833
youxuan388 <94452753+youxuan388@users.noreply.github.com>
2021-11-16 20:00:05 +0800

    第二堂-成績表
    成績表

---
+++ 第二堂-成績表.py
@@ -0,0 +1,11 @@
+y=float(input('輸入零到一百的任意數字'))
+if y<=100 and y>=90:
+    print('A')
+elif y<=89 and y>=80:
+    print('B')
+elif y<=79 and y>=70:
+    print('C')
+elif y<=69 and y>=60:
+    print('D')
+else:
+    print('E')
